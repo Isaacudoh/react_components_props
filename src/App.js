9755c3d30fce3a9a783739366b7c6985cfd0cdbb
nsikakudoh <nsikak@sparknerds.io>
2022-09-07 13:47:01 -0600

    add css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Movies from './Movies';
 function App() {
   return (
     <div className="App">
+      <img src='./logo192.png'></img>
       <Movies title="The Fellowship of the ring" hours="2" minutes="58" />
       <Movies title="The Two Towers" hours="2" minutes="59" />
       <Movies title="The Return of the King" hours="3" minutes="21" />
