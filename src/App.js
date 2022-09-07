a49bcfdaa596ef5ec21d444cdfc043625b9ed5e7
nsikakudoh <nsikak@sparknerds.io>
2022-09-07 13:20:41 -0600

    pass props

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Movies from './Movies';
 function App() {
   return (
     <div className="App">
-      <Movies />
-      <Movies />
-      <Movies />
+      <Movies title="The Fellowship of the ring" hours="2" minutes="58" />
+      <Movies title="The Two Towers" hours="2" minutes="59" />
+      <Movies title="The Return of the King" hours="3" minutes="21" />
     </div>
   );
 }
--- src/Movies.js
+++ src/Movies.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Movies() {
+function Movies(props) {
   return (
     <div>
-        <h1>Movie</h1>
+        <h1>{props.title}</h1>
+        <p># Hours: {props.hours}</p>
+        <p># Minutes: {props.minutes}</p>
+
     </div>
   )
 }
